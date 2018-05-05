d21a367e5fb93e328374b15ac85f4d619bd814a8
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-05 12:15:55 +1100

    Sauvegarde

--- .travis/scripts/artifactory_deploy.sh
+++ .travis/scripts/artifactory_deploy.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Paramètre du script
 groupId=com.calinfo-nc.services
 artifactId=common
 version=1.0-SNAPSHOT
 packaging=jar
 
+# Variable du programme
+baseUrl=https://calinfo.artifactoryonline.com/calinfo
+
+# Reconstitution de chemin de l'url dans Artifactory
 pathGroupId=$(echo $groupId | sed -e "s/\./\//g")
 pathArtifactId=$(echo $artifactId | sed -e "s/\./\//g")
-path=$pathGroupId/$pathArtifactId/version
-echo $path
+path=$pathGroupId/$pathArtifactId/$version
 
-#curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.jar" -T "$TRAVIS_BUILD_DIR/common/target/common-1.0-SNAPSHOT.jar"
-#curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.pom" -T "$TRAVIS_BUILD_DIR/common/pom.xml"
+# Reconstruction du nom de l'artifact dans Artifactory
+artifactName=$artifactId-$version
+
+echo "$baseUrl/ext-snapshot-local/$path/$artifactName.$packaging"
+
+if [$packaging != "pom" ]
+then
+    echo "publier pom"
+fi
+
+#curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "$baseUrl/ext-snapshot-local/$path/$artifactName.$packaging" -T "$TRAVIS_BUILD_DIR/common/target/common-1.0-SNAPSHOT.jar"
+#curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "$baseUrl/ext-snapshot-local/$path/$artifactName.pom" -T "$TRAVIS_BUILD_DIR/common/pom.xml"
