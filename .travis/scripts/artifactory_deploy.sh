f936242deabf45b90efe22a171c5660cbc4e0f7e
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-05 11:53:28 +1100

    Sauvegarde

--- .travis/scripts/artifactory_deploy.sh
+++ .travis/scripts/artifactory_deploy.sh
@@ -5,7 +5,7 @@ artifactId=$2
 version=$3
 packaging=$4
 
-path=$(echo $groupId | sed -e 's/\./\/')
+path=$(echo $groupId | sed -e "s/\./\/")
 echo $path
 
 #curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.jar" -T "$TRAVIS_BUILD_DIR/common/target/common-1.0-SNAPSHOT.jar"
