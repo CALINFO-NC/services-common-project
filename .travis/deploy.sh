70fb133d042bee6e8b1a48f10ca9950f3e733b63
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-06 10:31:31 +1100

    Sauvegarde

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+# Indiquer le lieu de stockage des script Travis
 export SCRIPT_BASE_DIR=$TRAVIS_BUILD_DIR/.travis-github/scripts
-source $SCRIPT_BASE_DIR/script_init.sh
 
+# Effectuer un déploiement du common sur artifactory
 artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
