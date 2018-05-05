8bc5c589e2884187c36d5b6e53e2ad2fa5aa3287
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-05 11:25:48 +1100

    Sauvegarde

--- .travis/scripts/artifactory_deploy.sh
+++ .travis/scripts/artifactory_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT \"https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.jar\" -T \"$TRAVIS_BUILD_DIR/common/target/common-1.0-SNAPSHOT.jar"
-curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT \"https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.pom\" -T \"$TRAVIS_BUILD_DIR/common/pom.xml"
+curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.jar" -T "$TRAVIS_BUILD_DIR/common/target/common-1.0-SNAPSHOT.jar"
+curl -u $ARTIFACTORY_USER:$ARTIFACTORY_TOKEN -X PUT "https://calinfo.artifactoryonline.com/calinfo/ext-snapshot-local/com/calinfo-nc/services/common/1.0-SNAPSHOT/common-1.0-SNAPSHOT.pom" -T "$TRAVIS_BUILD_DIR/common/pom.xml"
