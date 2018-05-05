0c3ca3c21e8ef54f4063398a4a5b399524bdb9f9
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-06 01:57:34 +1100

    Sauvegarde

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 commonPathPomFile=$1
 commonBbuildFileName=$2
 
-export SCRIPT_BASE_DIR=.travis-github/scripts
+export SCRIPT_BASE_DIR=$TRAVIS_BUILD_DIR/.travis-github/scripts
 
 source $SCRIPT_BASE_DIR/script_init.sh
 
