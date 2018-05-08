cb1e0f39dbf22ad48e0abaa432edf6e91a4568cb
David ALEXIS <david.alexis@calinfo-nc.com>
2018-05-08 17:06:50 +1100

    Sauvegarde

---
+++ common/src/test/java/com/calinfo/api/common/manager/RestTemplateManagerGetRestTemplate.java
@@ -0,0 +1,27 @@
+package com.calinfo.api.common.manager;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by dalexis on 08/05/2018.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestTemplateManagerGetRestTemplate {
+
+    @Autowired
+    private RestTemplateManager restTemplateManager;
+
+
+    @Test
+    public void testGetPrincipal(){
+        RestTemplate restTemplate = restTemplateManager.getRestTemplate();
+        Assert.assertEquals(restTemplate, restTemplateManager.getRestTemplate());
+    }
+}
