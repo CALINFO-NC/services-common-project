9509ec49eab140d4bb67f15185e275b9846c3aca
David ALEXIS <david.alexis@calinfo-nc.com>
2018-07-16 18:23:24 +1100

    Sauvegarde

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,13 +4,13 @@
 source $SCRIPT_BASE_DIR/scripts/artifactory.sh
 
 # Effectuer les déploiements sur artifactory
-artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
-artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-1.0-SNAPSHOT.jar
-artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
+#artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
+#artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-1.0-SNAPSHOT.jar
+#artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
 
 # Publier le site si nécessaire
-isSnapshot=$(maven_is_snapshot "$TRAVIS_BUILD_DIR/pom.xml")
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$isSnapshot" == "false" ] || [ "$TRAVIS_BRANCH" == "master" ]
-then
-    mvn site -B
-fi
+#isSnapshot=$(maven_is_snapshot "$TRAVIS_BUILD_DIR/pom.xml")
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$isSnapshot" == "false" ] || [ "$TRAVIS_BRANCH" == "master" ]
+#then
+    #mvn site -B
+#fi
