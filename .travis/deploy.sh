0996c0389ab5166c47dd9356137840cde55befe6
David ALEXIS <david.alexis@calinfo-nc.com>
2018-08-27 03:19:20 +1100

    Modification du script de déploiement

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,12 @@
 source $SCRIPT_BASE_DIR/scripts/artifactory.sh
 
 # Effectuer les déploiements sur artifactory
-artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
-artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-1.0-SNAPSHOT.jar
-artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-1.0-SNAPSHOT.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
+fi
 
 # Publier le site si nécessaire
 isSnapshot=$(maven_is_snapshot "$TRAVIS_BUILD_DIR/pom.xml")
