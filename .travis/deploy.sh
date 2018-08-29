9f86ccdedc57969dc92bfc18f4d4291ff19fc441
David ALEXIS <david.alexis@calinfo-nc.com>
2018-08-29 11:47:39 +1100

    Sauvegarde

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,8 @@ source $SCRIPT_BASE_DIR/scripts/artifactory.sh
 # Effectuer les déploiements sur artifactory
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
-    artifactory_deploy $TRAVIS_BUILD_DIR/common common-1.0-SNAPSHOT.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-1.0-SNAPSHOT.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common common.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api.jar
     artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
 fi
 
