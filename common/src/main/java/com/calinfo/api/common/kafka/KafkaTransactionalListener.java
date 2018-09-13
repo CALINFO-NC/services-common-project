f6b193bb733d05de291a3d467041897a2b5bc080
jabab <jabab@tuta.io>
2018-09-13 16:33:45 +1100

    feat: kafka en mode non bloquant

--- common/src/main/java/com/calinfo/api/common/kafka/KafkaTransactionalListener.java
+++ common/src/main/java/com/calinfo/api/common/kafka/KafkaTransactionalListener.java
@@ -2,21 +2,23 @@ package com.calinfo.api.common.kafka;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.KafkaFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 
 @Component
 @ConditionalOnProperty(value = "common.configuration.kafka-event.enabled")
@@ -27,7 +29,7 @@ public class KafkaTransactionalListener {
     private static final int TOPIC_PARTITION = 1;
     private static final short TOPIC_REPLICA = 1;
 
-    private Set<String> existingTopics = new HashSet<>();
+    private final Set<String> existingTopics = new HashSet<>();
 
     @Autowired
     private KafkaAdmin kafkaAdmin;
@@ -37,47 +39,71 @@ public class KafkaTransactionalListener {
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
-    public void call(KafkaEvent kafkaEvent){
+    public void call(KafkaEvent kafkaEvent) {
 
         log.info("Prepare topic '{}' to send", kafkaEvent.getTopic());
 
-        try {
+        String topicName = kafkaEvent.getTopic();
 
-            String topicName = kafkaEvent.getTopic();
-            if (!existingTopics.contains(topicName)){
-                registerTopic(topicName);
-            }
+        if (existingTopics.contains(topicName)) {
 
             kafkaTemplate.send(topicName, kafkaEvent);
 
-        } catch (ExecutionException e) {
-            log.error(e.getMessage(), e);
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
-            Thread.currentThread().interrupt();
-        }
-    }
+        } else {
 
-    private void registerTopic(String topicName) throws ExecutionException, InterruptedException {
+            try (AdminClient client = AdminClient.create(kafkaAdmin.getConfig())) {
 
-        try (AdminClient client = AdminClient.create(kafkaAdmin.getConfig())) {
+                client.listTopics()
+                        .names()
+                        .thenApply(createTopicIfNeeded(kafkaEvent, topicName, client));
 
-            Set<String> clientTopics = client.listTopics().names().get();
-
-            if (existingTopics.isEmpty()) {
-                existingTopics.addAll(clientTopics);
             }
 
-            if (!existingTopics.contains(topicName)) {
+        }
+
+
+    }
+
+    private KafkaFuture.Function<Set<String>, Object> createTopicIfNeeded(KafkaEvent kafkaEvent, String topicName, AdminClient client) {
+        return new KafkaFuture.Function<Set<String>, Object>() {
+
+            @Override
+            public Object apply(Set<String> topicNames) {
+
+                existingTopics.addAll(topicNames);
+
+                if (existingTopics.contains(topicName)) {
+
+                    kafkaTemplate.send(topicName, kafkaEvent);
+
+                } else {
+
+                    client.createTopics(Collections.singleton(new NewTopic(topicName, TOPIC_PARTITION, TOPIC_REPLICA)))
+                            .all()
+                            .thenApply(sendMessageAfterTopicCreation(topicName, kafkaEvent));
+                }
+
+                return null;
+            }
+        };
+    }
+
+
+    private KafkaFuture.Function<Void, ListenableFuture<SendResult<String, Object>>> sendMessageAfterTopicCreation(final String topicName, final KafkaEvent kafkaEvent) {
+
+        return new KafkaFuture.Function<Void, ListenableFuture<SendResult<String, Object>>>() {
+
+            @Override
+            public ListenableFuture<SendResult<String, Object>> apply(Void aVoid) {
+
                 log.info("Register topic '{}' in Kafka", topicName);
 
                 existingTopics.add(topicName);
-                NewTopic newTopic = new NewTopic(topicName, TOPIC_PARTITION, TOPIC_REPLICA);
-                // le .all.get sont la pour attendre la création du topic
-                // FIXME Il faudrait le faire en asynchrone, avec dans le "then" ajouter la publication de l'évenement ?
-                client.createTopics(Collections.singleton(newTopic)).all().get();
+
+                return kafkaTemplate.send(topicName, kafkaEvent);
+
             }
-        }
+        };
     }
 
 }
