2d770b9a9fccba1fc282c4a433665b937685dd58
David ALEXIS <david.alexis@calinfo-nc.com>
2018-09-22 09:09:05 +1100

    Modification des annotation sur le MediaDto

--- common-api/src/main/java/com/calinfo/api/common/dto/MediaDto.java
+++ common-api/src/main/java/com/calinfo/api/common/dto/MediaDto.java
@@ -1,12 +1,16 @@
 package com.calinfo.api.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class MediaDto implements Dto {
@@ -14,10 +18,10 @@ public class MediaDto implements Dto {
     @NotBlank
     @NotEmpty
     @NotNull
-    private String base64;
+    private String contentType;
 
     @NotBlank
     @NotEmpty
     @NotNull
-    private String contentType;
+    private String base64;
 }
