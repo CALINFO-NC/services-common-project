f61e2df6c76e09a33b3dfc1b6d0e4dfd615ed938
David ALEXIS <david.alexis@calinfo-nc.com>
2018-09-22 23:08:54 +1100

    Modification des propriétés syr DynamicListDto

--- common-api/src/main/java/com/calinfo/api/common/dto/DynamicListDto.java
+++ common-api/src/main/java/com/calinfo/api/common/dto/DynamicListDto.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -15,8 +16,14 @@ import java.util.function.Function;
 @NoArgsConstructor
 @Getter
 @Setter
-public class DynamicListDto<R extends Dto> implements Dto {
+public class DynamicListDto<R extends Dto> implements Dto, MessageInfoAndWarningInterface {
 
+
+    @Size(min = 0, max = 0)
+    private List<String> listInfoMessages = new ArrayList<>();
+
+    @Size(min = 0, max = 0)
+    private List<String> listWarningMessages = new ArrayList<>();
 
     private List<R> data = new ArrayList<>();
 
