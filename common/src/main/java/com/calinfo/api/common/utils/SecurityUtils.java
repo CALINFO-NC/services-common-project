b5449a7839725557069181a45064c02395dfb840
David ALEXIS <david.alexis@calinfo-nc.com>
2019-06-15 11:40:05 +1100

    Micro restructuration de la sécurité

--- common/src/main/java/com/calinfo/api/common/utils/SecurityUtils.java
+++ common/src/main/java/com/calinfo/api/common/utils/SecurityUtils.java
@@ -1,11 +1,17 @@
 package com.calinfo.api.common.utils;
 
+import com.calinfo.api.common.ex.MessageStatusException;
+import com.calinfo.api.common.security.AbstractCommonPrincipal;
+import com.calinfo.api.common.security.CommonPrincipal;
 import com.calinfo.api.common.security.JwtUser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.xml.bind.DatatypeConverter;
 import java.security.KeyFactory;
@@ -16,6 +22,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by dalexis on 20/11/2017.
@@ -106,5 +113,33 @@ public class SecurityUtils {
         }
 
         return tokenUser;
+    }
+
+    /**
+     * Retourne le principal associé au token
+     *
+     * @param token Token à décrypter
+     * @param apiKey Clé d'api permettant de renouveller le token
+     * @param publicKey Clé public
+     * @return
+     */
+    public static AbstractCommonPrincipal getPrincipalFromTokens(String token, String apiKey, String publicKey){
+
+        JwtUser user;
+        try{
+            user = SecurityUtils.getUserFromJwt(token, publicKey);
+        }
+        catch(InvalidKeySpecException | NoSuchAlgorithmException e){
+            throw new MessageStatusException(HttpStatus.FORBIDDEN, "JWT Expired or invalid");
+        }
+
+        List<String> lstRoles = user.getRoles();
+        if (lstRoles == null){
+            lstRoles = new ArrayList<>();
+        }
+        List<? extends GrantedAuthority> authorities = lstRoles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+        // Création du principal
+        return new CommonPrincipal(apiKey, token, user.getDomain(), user.getLogin(), "", authorities);
     }
 }
