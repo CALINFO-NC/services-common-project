28c4eb25c6a1a425e0abe6b63559782411f89c1b
David ALEXIS <david.alexis@calinfo-nc.com>
2019-07-19 11:07:08 +1100

    Ajout de la gestion de la sécurité

--- common/src/test/java/com/calinfo/api/common/MocksApplication.java
+++ common/src/test/java/com/calinfo/api/common/MocksApplication.java
@@ -2,11 +2,12 @@ package com.calinfo.api.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableAsync(mode = AdviceMode.ASPECTJ)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MocksApplication {
