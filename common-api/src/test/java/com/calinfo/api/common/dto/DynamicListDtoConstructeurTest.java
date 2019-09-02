b9e7d0222abee6d07bec66b787f4dcd50a0970cb
David ALEXIS <david.alexis@calinfo-nc.com>
2019-09-03 09:07:27 +1100

    Ajout fonctionnalité au DynmaicListDto

--- common-api/src/test/java/com/calinfo/api/common/dto/DynamicListDtoConstructeurTest.java
+++ common-api/src/test/java/com/calinfo/api/common/dto/DynamicListDtoConstructeurTest.java
@@ -16,7 +16,7 @@ public class DynamicListDtoConstructeurTest {
 
 
     @Test
-    public void call(){
+    public void callPage(){
 
         List<Integer> lst = Arrays.asList(3, 4);
 
@@ -34,5 +34,40 @@ public class DynamicListDtoConstructeurTest {
         Assert.assertTrue(dyn.getData().size() == 2);
         Assert.assertTrue(dyn.getData().stream().anyMatch(d -> d.getValue().equals("3")));
         Assert.assertTrue(dyn.getData().stream().anyMatch(d -> d.getValue().equals("4")));
+    }
+
+    @Test
+    public void callGlobalList(){
+
+        List<Integer> lst = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        DynamicListDto<Integer> dyn = new DynamicListDto(lst, 0, 3);
+
+        Assert.assertEquals(10, dyn.getCount());
+        Assert.assertEquals(3, dyn.getData().size());
+        Assert.assertTrue(dyn.getData().contains(1));
+        Assert.assertTrue(dyn.getData().contains(2));
+        Assert.assertTrue(dyn.getData().contains(3));
+
+        dyn = new DynamicListDto(lst, 1, 3);
+
+        Assert.assertEquals(10, dyn.getCount());
+        Assert.assertEquals(3, dyn.getData().size());
+        Assert.assertTrue(dyn.getData().contains(4));
+        Assert.assertTrue(dyn.getData().contains(5));
+        Assert.assertTrue(dyn.getData().contains(6));
+
+        dyn = new DynamicListDto(lst, 4, 2);
+
+        Assert.assertEquals(10, dyn.getCount());
+        Assert.assertEquals(2, dyn.getData().size());
+        Assert.assertTrue(dyn.getData().contains(9));
+        Assert.assertTrue(dyn.getData().contains(10));
+
+        dyn = new DynamicListDto(lst, 3, 3);
+
+        Assert.assertEquals(10, dyn.getCount());
+        Assert.assertEquals(1, dyn.getData().size());
+        Assert.assertTrue(dyn.getData().contains(10));
     }
 }
