f0c6eed3c7a531b6df370c43c30b871a8f612726
David ALEXIS <david.alexis@calinfo-nc.com>
2019-10-10 22:25:46 +1100

    Ajout de classifier dans le déploiement

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,15 +7,15 @@ source $SCRIPT_BASE_DIR/scripts/artifactory.sh
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
     artifactory_deploy $TRAVIS_BUILD_DIR/common common.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common common-javadoc-resources.jar javadoc
-    artifactory_deploy $TRAVIS_BUILD_DIR/common common-sources.jar sources
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-javadoc-resources.jar javadoc
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-sources.jar sources
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test-javadoc-resources.jar javadoc
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test-sources.jar sources
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common common-javadoc-resources.jar javadoc
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common common-sources.jar sources
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api.jar
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-javadoc-resources.jar javadoc
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-sources.jar sources
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test.jar
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test-javadoc-resources.jar javadoc
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test-sources.jar sources
+    #artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
 fi
 
 # Publier le site si nécessaire
