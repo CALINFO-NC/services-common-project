144ecce51b287be8d01804e7cffb960c4f1b569f
David ALEXIS <david.alexis@calinfo-nc.com>
2019-10-10 20:25:58 +1100

    correction du deploy pour le master

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,21 +7,21 @@ source $SCRIPT_BASE_DIR/scripts/artifactory.sh
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
     artifactory_deploy $TRAVIS_BUILD_DIR/common common.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common common-javadoc-resources.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common common-sources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-javadoc-resources common-javadoc-resources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-sources common-sources.jar
     artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-javadoc-resources.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-api-sources.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-test.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-test-javadoc-resources.jar
-    artifactory_deploy $TRAVIS_BUILD_DIR/common-api common-test-sources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-api-javadoc-resources common-api-javadoc-resources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-api-sources common-api-sources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-test common-test.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-test-javadoc-resources common-test-javadoc-resources.jar
+    artifactory_deploy $TRAVIS_BUILD_DIR/common-test-sources common-test-sources.jar
     artifactory_deploy $TRAVIS_BUILD_DIR/common-libs pom.xml
 fi
 
 # Publier le site si nécessaire
 # isSnapshot=$(maven_is_snapshot "$TRAVIS_BUILD_DIR/pom.xml")
 # if [ "$TRAVIS_BRANCH" == "master" ] && [ "$isSnapshot" == "false" ]
-#if [ "$TRAVIS_BRANCH" == "master" ]
-#then
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
     mvn site -B
-#fi
+fi
