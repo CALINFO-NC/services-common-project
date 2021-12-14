4cda830604cabac7ae1ae5c2cd23c2466bbfde7d
David ALEXIS <david.alexis@calinfo-nc.com>
2021-12-14 12:52:00 +1100

    Correction sonnar

--- common-api/src/main/java/com/calinfo/api/common/dto/MediaDto.java
+++ common-api/src/main/java/com/calinfo/api/common/dto/MediaDto.java
@@ -25,7 +25,6 @@ package com.calinfo.api.common.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
